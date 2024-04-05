ce574355392a5a50ffbeee04df47753e93b54f37
saaketh <saakethkvm@gmail.com>
2024-04-05 09:23:29 +0530

    updated code with mongo DB connection

--- Quiz/src/main/java/com/Saaketh/Quiz/Models/Question.java
+++ Quiz/src/main/java/com/Saaketh/Quiz/Models/Question.java
@@ -1,16 +1,18 @@
 package com.Saaketh.Quiz.Models;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
-@Entity
+@Getter
+@Setter
+@Document(collection="Question")
 public class Question {
 	
 	@Id
@@ -24,5 +26,24 @@ public class Question {
 	private String rightAnswer;
 	private String difficultyLevel;
 	private String category;
+	
+	public Question(Integer id, String questiontitle, String option1, String option2, String option3, String option4,
+			String rightAnswer, String difficultyLevel, String category) {
+		super();
+		this.id = id;
+		this.questiontitle = questiontitle;
+		this.option1 = option1;
+		this.option2 = option2;
+		this.option3 = option3;
+		this.option4 = option4;
+		this.rightAnswer = rightAnswer;
+		this.difficultyLevel = difficultyLevel;
+		this.category = category;
+	}
+	
+	public Question()
+	{
+		
+	}
 
 }
--- Quiz/src/main/java/com/Saaketh/Quiz/Repositories/QuestionRepository.java
+++ Quiz/src/main/java/com/Saaketh/Quiz/Repositories/QuestionRepository.java
@@ -2,20 +2,22 @@ package com.Saaketh.Quiz.Repositories;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Saaketh.Quiz.Models.Question;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question,Integer>{
+public interface QuestionRepository extends MongoRepository<Question,Integer>{
 
 
 	List<Question> findByCategory(String category);
 
-	@Query(value = "SELECT * FROM question q where q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
-	List<Question> findRandomQuestions(String category, int numQ);
+
 
 
 }
+
+
+//@Query(value = "SELECT * FROM question q where q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
+//List<Question> findRandomQuestions(String category, int numQ);
